f8215c4325de09ec25db4b67b42dbba7cec98521
Robert Hall <langbert@gmail.com>
2020-07-21 21:28:26 -0600

    Initial commit

---
+++ packages/google-spreadsheets/rollup.config.js
@@ -0,0 +1,26 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+// import { builtinModules } from 'module'
+// import { dependencies } from './package.json'
+
+const { NODE_ENV } = process.env
+const prod = NODE_ENV === 'production'
+
+export default {
+	input: 'src/index.js',
+	output: {
+		file: 'dist/index.js',
+		format: 'cjs',
+	},
+	// external: [...builtinModules, ...Object.keys(dependencies)],
+	plugins: [
+		resolve(),
+		commonjs(),
+		prod && terser(),
+	],
+	// onwarn: (warning, onwarn) => (
+	// 	warning.code === 'CIRCULAR_DEPENDENCY'
+	// 	&& /[/\\]@plugin[/\\]/.test(warning.message)
+	// ) || onwarn(warning),
+}
